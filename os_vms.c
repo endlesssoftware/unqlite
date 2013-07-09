746d00c5b316deece037d65c44003299dc4beb97
Tim E. Sneddon <tim@sneddon.id.au>
2013-07-09 15:25:04 +0800

    Notes and start of an OpenVMS module.

---
+++ os_vms.c
@@ -0,0 +1,40 @@
+/*
+** This vector defines all the methods that can operate on an
+** unqilte_file for OpenVMS.
+*/
+static const unqlite_io_methods vmsIoMethod = {
+  1,                              /* iVersion */
+  vmsClose,                       /* xClose */
+  vmsRead,                        /* xRead */
+  vmsWrite,                       /* xWrite */
+  vmsTruncate,                    /* xTruncate */
+  vmsSync,                        /* xSync */
+  vmsFileSize,                    /* xFileSize */
+  vmsLock,                        /* xLock */
+  vmsUnLock,                      /* xUnlock */
+  vmsCheckReservedLock,           /* xCheckReservedLock */
+  vmsSectorSize,                  /* xSectorSize */
+};
+
+int unqlite_os_init(void){
+  static unqlite_vfs vmsVfs = {
+    "vms",               /* zName */
+    1,                   /* iVersion */
+    sizeof(vmsFile),     /* szOsFile */
+#if defined(NAML$C_MAXRSS)
+    NAML$C_MAXRSS,       /* mxPathname */
+#else
+    NAM$C_MAXRSS,        /* mxPathname */
+#endif
+    vmsOpen,             /* xOpen */
+    vmsDelete,           /* xDelete */
+    vmsAccess,           /* xAccess */
+    vmsFullPathname,     /* xFullPathname */
+    vmsTmpDir,		 /* xTmpDir */
+    vmsSleep,            /* xSleep */
+    vmsCurrentTime,      /* xCurrentTime */
+    vmsGetLastError,     /* xGetLastError */
+  };
+
+  return UNQLITE_OK;
+}
--- readme.txt
+++ readme.txt
@@ -5,3 +5,24 @@
 
     Much of the OpenVMS-sepecific code contained in this project has been
  built from the port of SQLite.
+
+---
+
+ Configuration options to put into DESCRIP.MMS
+
+  - UNQLITE_ENABLE_THREADS
+  - JX9_ENABLE_MATH_FUNC
+  - UNQLITE_ENABLE_JX9_HASH_IO
+  
+
+ Run-time options (need to figure out setting these at compile):
+
+  - #define UNQLITE_LIB_CONFIG_USER_MALLOC            1 /* ONE ARGUMENT: const SyMemMethods *pMemMethods */
+  - #define UNQLITE_LIB_CONFIG_USER_MUTEX             3 /* ONE ARGUMENT: const SyMutexMethods *pMutexMethods */
+  - #define UNQLITE_LIB_CONFIG_VFS                    6 /* ONE ARGUMENT: const unqlite_vfs *pVfs */
+  - #define UNQLITE_LIB_CONFIG_PAGE_SIZE              8 /* ONE ARGUMENT: int iPageSize */
+
+ What about the Jx9 stuff?  It has a VFS interface that wants to know
+ how to set default, etc.
+
+ Make sure __UNIXES__ and __WINNT__ are #undef'd
